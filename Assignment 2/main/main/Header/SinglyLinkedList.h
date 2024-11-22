b6832647a21e6879f3811442cc808f3a0e1e7df2
Edwin <89662466+SumBiqBoi@users.noreply.github.com>
2024-11-21 21:19:19 -0500

    Created the base for the linked list

--- Assignment 2/main/main/Header/SinglyLinkedList.h
+++ Assignment 2/main/main/Header/SinglyLinkedList.h
@@ -2,7 +2,38 @@
 #include <cassert>
 
 template <class T>
-class SinglyLinkedList
+class LinkedListNode
 {
+public:
+	int priority;
+	LinkedListNode* data;
+	LinkedListNode* next;
+};
 
+template <class T>
+class LinkedList
+{
+public:
+	LinkedList(): size(0), root(nullptr), lastNode(nullptr) {}
+	~LinkedList() {}
+
+	LinkedListNode<T>* Start()
+	{
+		assert(root != nullptr);
+		return root;
+	}
+	LinkedListNode<T>* End()
+	{
+		return nullptr;
+	}
+	LinkedListNode<T>* Last()
+	{
+		assert(lastNode != nullptr);
+		return lastNode;
+	}
+
+private:
+	int = size;
+	LinkedListNode<T>* root;
+	LinkedListNode<T>* lastNode;
 };
